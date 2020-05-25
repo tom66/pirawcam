25f56d8a3146d48f27ad6d2e51387e4da4033aa9
Tom Oldbury <toldbury@gmail.com>
2020-05-25 14:28:27 +0100

    fix memory puking

--- rawcam.c
+++ rawcam.c
@@ -72,7 +72,7 @@ enum teardown { NONE=0, PORT, POOL, C1, C2 };
 			return false;	      \
 		}} while(0)
 
-#define RAWCAM_VERSION 	"v0.0.5"
+#define RAWCAM_VERSION 	"v0.0.6"
 
 int mmal_ret_status = 0;
 int fi_counter = 0;
