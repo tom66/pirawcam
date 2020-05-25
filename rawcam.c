6cbc1cc1751772523e62adc977ff1913d59e1ecf
Tom Oldbury <toldbury@gmail.com>
2020-05-25 16:06:13 +0100

    try with CODECSIDEINFO buffers

--- rawcam.c
+++ rawcam.c
@@ -122,13 +122,13 @@ void rawcam_stop (void) {
 
 static void callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
 	assert (r.running);
-	if (!(buffer->flags&MMAL_BUFFER_HEADER_FLAG_CODECSIDEINFO)) {
+	//if (!(buffer->flags&MMAL_BUFFER_HEADER_FLAG_CODECSIDEINFO)) {
 		fprintf(stderr,"queueing buffer %p (data %p, len %d, flags %02x)\n", buffer, buffer->data, buffer->length, buffer->flags);
 		mmal_queue_put(r.queue, buffer);
 		poke_efd (1);
-	} else {
-		rawcam_buffer_free(buffer);
-	}
+	//} else {
+	//	rawcam_buffer_free(buffer);
+	//}
 }
 
 PyObject *rawcam_get_memoryview_from_buffer(MMAL_BUFFER_HEADER_T *buffer) {
